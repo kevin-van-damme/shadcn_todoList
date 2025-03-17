c7d9af66db2c6f8014f635e569cde0ce2310cd17
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-03-17 11:11:56 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const App = () => {
+  return (
+    <div className="space-y-4 p-4">
+      <h1 className="bg-amber-400 p-4 text-3xl font-bold text-amber-50">App</h1>
+      <Button>Click me</Button>
+      <hr />
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It's animated by default, but you can disable it if you prefer.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+export default App;
