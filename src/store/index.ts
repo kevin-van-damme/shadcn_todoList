73c60a76214ba851a3ea5a1ed1bf44ab8b766c9b
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-03-19 00:25:51 +0100

    reverting to ed9a02c

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import paginationReducer from "./paginationSlice";
+import filterReducer from "./filterSlice";
 import todosApi from "./todosApi";
 
 const store = configureStore({
   reducer: {
     [todosApi.reducerPath]: todosApi.reducer,
+    pagination: paginationReducer,
+    filter: filterReducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(todosApi.middleware);
@@ -11,4 +15,5 @@ const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
